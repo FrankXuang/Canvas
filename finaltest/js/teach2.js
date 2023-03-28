a8245a558d3cf5f698793653ac139875fb7710ed
frank <frank120910@gmail.com>
2023-03-28 15:04:03 +0800

    add

---
+++ finaltest/js/teach2.js
@@ -0,0 +1,40 @@
+var canvas = document.querySelector("canvas");
+var tilesetContainer = document.querySelector(".tileset-container");
+var tilesetSelection = document.querySelector(".tileset-container_selection");
+var tilesetImage = document.querySelector("#tileset-source");
+
+var selection = [0, 0]; //Which tile we will paint from the menu
+
+var isMouseDown = false;
+var currentLayer = 0;
+var layers = [
+    //Bottom
+    {
+        //Structure is "x-y": ["tileset_x", "tileset_y"]
+        //EXAMPLE: "1-1": [3, 4],
+    },
+    //Middle
+    {},
+    //Top
+    {}
+];
+
+function draw() {
+    var ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+
+    //px
+    var size_of_crop = 48;
+}
+
+tilesetImage.onload = function () {
+    layers = defaultState;
+    draw();
+    setLayer(0);
+}
+
+
+
+
+tilesetImage.src = "https://assets.codepen.io/21542/TileEditorSpritesheet.2x_2.png";
---
+++ finaltest/recording-canvas-drawimage/README.md
@@ -0,0 +1,8 @@
+# Recording: Canvas drawImage()
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/punkydrewster713/pen/ExPLXGE](https://codepen.io/punkydrewster713/pen/ExPLXGE).
+
+This is the code for my tutorial video showing different usages of HTML Canvas `drawImage`.
+See the video here:
+
+https://youtu.be/jEUuM5bRAzw
